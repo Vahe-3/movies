e32c1aa372f8df68f60072349ce7ffe2915a2520
Vahe-3 <vahe.avetisyan333@gmail.com>
2022-04-14 15:47:49 +0400

    add api-key

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+const API_KEY = process.env.REACT_APP_API_KEY;
+
 export const findMovies = (movieName, type="all") => {
-    return axios.get(`https://www.omdbapi.com/?apikey=a3f64d98&s=${movieName}${type !== "all" ?  `&type=${type}` : ""}`);
+    return axios.get(`https://www.omdbapi.com/?apikey=${API_KEY}=${movieName}${type !== "all" ?  `&type=${type}` : ""}`);
 }
